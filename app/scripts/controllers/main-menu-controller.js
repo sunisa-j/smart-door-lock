b6f7e80d5db14d5ff4316b632e09923d2ef3931b
sunisa <sunisa.j@icloud.com>
2015-06-03 17:11:34 +0700

    Create test class for red color

--- app/scripts/controllers/main-menu-controller.js
+++ app/scripts/controllers/main-menu-controller.js
@@ -2,4 +2,13 @@
 
 window.app.controller('MainMenuController', function ($scope) {
 
+    // get login name
+    $scope.loginName = "สุนิสา จุลรัตน์";
+
+    // get count of unread message (notification)
+    $scope.unreadMsg = 20;
+
+    // if this user is approver. get count of approve msg number
+    $scope.approveMsg = 32;
+
 });
