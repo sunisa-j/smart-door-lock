7d05ac339a1b5dcb04c58896326a1c3db3997053
sunisa <sunisa.j@icloud.com>
2015-06-04 14:50:47 +0700

    Create Enable Card Popup Function

--- app/scripts/controllers/settings-controller.js
+++ app/scripts/controllers/settings-controller.js
@@ -1,5 +1,21 @@
 'use strict';
 
-window.app.controller('SettingsController', function ($scope) {
+window.app.controller('SettingsController', function ($scope, $ionicPopup) {
+
+    $scope.confirmEnableCard = function() {
+
+        var myPopup = $ionicPopup.show({
+            title: "Confirm",
+            template: 'Are you sure to enable this card ?',
+            buttons: [{
+                text: 'Cancel',
+                type: 'button-outline button-stable'
+                },{
+                text: 'Confirm',
+                type: 'button-outline button-balanced'
+            }
+            ]
+        })
+    }
 
 });
