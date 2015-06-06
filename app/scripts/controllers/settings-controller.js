462f6851d18c232306588ac588fe99ec3abd9c71
sunisa <sunisa.j@icloud.com>
2015-06-07 00:52:20 +0700

    organize code

--- app/scripts/controllers/settings-controller.js
+++ app/scripts/controllers/settings-controller.js
@@ -7,23 +7,24 @@ window.app.controller('SettingsController', function ($scope, $ionicPopup) {
         var myPopup = $ionicPopup.show({
             title: "Confirm",
             template: 'Are you sure to enable this card ?',
-            buttons: [{
-                text: '<div class="flex align-items-center">' +
-                            '<span class="flex-basis-30">' +
-                                '<i class="button-icon-size ion-ios-close-outline"></i>' +
-                            '</span>' +
-                            '<span class="flex-1">Cancel</span>' +
-                        '</div>',
-                type: 'button-outline button-stable'
+            buttons: [
+                {
+                    text: '<div class="flex align-items-center">' +
+                                '<span class="flex-basis-30">' +
+                                    '<i class="button-icon-size ion-ios-close-outline"></i>' +
+                                '</span>' +
+                                '<span class="flex-1">Cancel</span>' +
+                            '</div>',
+                    type: 'button-outline button-stable'
                 },{
-                text: '<div class="flex align-items-center">' +
-                            '<span class="flex-basis-30">' +
-                                '<i class="button-icon-size ion-ios-checkmark-outline"></i>' +
-                            '</span>' +
-                            '<span class="flex-1">Confirm</span>' +
-                '       </div>',
-                type: 'button-outline button-balanced'
-            }
+                    text: '<div class="flex align-items-center">' +
+                                '<span class="flex-basis-30">' +
+                                    '<i class="button-icon-size ion-ios-checkmark-outline"></i>' +
+                                '</span>' +
+                                '<span class="flex-1">Confirm</span>' +
+                            '</div>',
+                    type: 'button-outline button-balanced'
+                }
             ]
         })
     }
