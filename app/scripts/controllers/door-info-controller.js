c1dc7f6e9a9501a77779fa190f87e978c7b97684
sunisa <sunisa.j@icloud.com>
2015-06-07 00:52:47 +0700

    add $scope.doorMenu

--- app/scripts/controllers/door-info-controller.js
+++ app/scripts/controllers/door-info-controller.js
@@ -1,5 +1,9 @@
 'use strict';
 
-window.app.controller('DoorInfoController', function () {
+window.app.controller('DoorInfoController', function ($scope) {
+
+    $scope.doorMenu = {
+        name: 'doorInfo'
+    };
 
 });
