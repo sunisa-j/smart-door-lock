4c82efc4fd2d235f7c2339934d6832816f724658
sunisa <sunisa.j@icloud.com>
2015-06-08 15:33:15 +0700

    Add Comment

--- app/scripts/controllers/door-info-controller.js
+++ app/scripts/controllers/door-info-controller.js
@@ -2,6 +2,7 @@
 
 window.app.controller('DoorInfoController', function ($scope) {
 
+    // Set Default Menu (Menu contains are 'doorInfo', 'configDoor', 'log' and 'manageUser')
     $scope.doorMenu = {
         name: 'doorInfo'
     };
