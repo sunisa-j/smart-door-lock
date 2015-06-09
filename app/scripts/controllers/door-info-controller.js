ec217c735c3ea3aaf1816d2a880b68d8945a4d30
sunisa <sunisa.j@icloud.com>
2015-06-09 17:06:35 +0700

    Add confirm delete box

--- app/scripts/controllers/door-info-controller.js
+++ app/scripts/controllers/door-info-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.app.controller('DoorInfoController', function ($scope) {
+window.app.controller('DoorInfoController', function ($scope, $ionicPopup) {
 
     // Set Default Menu (Menu contains are 'doorInfo', 'configDoor', 'log' and 'manageUser')
     $scope.doorMenu = {
@@ -12,4 +12,39 @@ window.app.controller('DoorInfoController', function ($scope) {
         value: false
     };
 
+    $scope.deleteSelected = function() {
+        $scope.showDelButton.value = false;
+        console.log("deleted");
+    };
+
+    // Confirm Delete Box
+    $scope.confirmDelBox = function() {
+
+        var myPopup = $ionicPopup.show({
+            title: "Confirm",
+            template: 'Do you confirm to delete all user you selected ?',
+            buttons: [
+                {
+                    text: '<div class="flex align-items-center">' +
+                    '<span class="flex-basis-30">' +
+                    '<i class="button-icon-size ion-ios-close-outline"></i>' +
+                    '</span>' +
+                    '<span class="flex-1">Cancel</span>' +
+                    '</div>',
+                    type: 'button-outline button-stable'
+                },{
+                    text: '<div class="flex align-items-center">' +
+                    '<span class="flex-basis-30">' +
+                    '<i class="button-icon-size ion-ios-checkmark-outline"></i>' +
+                    '</span>' +
+                    '<span class="flex-1">Confirm</span>' +
+                    '</div>',
+                    type: 'button-outline button-balanced',
+                    onTap: function() {
+                        $scope.deleteSelected();
+                    }
+                }
+            ]
+        });
+    }
 });
