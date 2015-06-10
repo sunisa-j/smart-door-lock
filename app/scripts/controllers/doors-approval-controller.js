027202a2e4fcd1a377e8cdae45c81443efdf6e83
sunisa <sunisa.j@icloud.com>
2015-06-10 17:58:19 +0700

    Create doors approval controller

---
+++ app/scripts/controllers/doors-approval-controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+window.app.controller('DoorsApprovalController', function ($scope) {
+
+    // Set Default Menu (Menu contains are 'waitingApprove' and 'approved')
+    $scope.doorApproveMenu = {
+        name: 'waitingApprove'
+    };
+
+});
