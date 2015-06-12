856528b3251b7a1ba4f7945afb92b9bff292c50d
sunisa <sunisa.j@icloud.com>
2015-06-12 15:07:56 +0700

    Add doors-access service and get doors user can access

--- app/scripts/controllers/doors-controller.js
+++ app/scripts/controllers/doors-controller.js
@@ -1,5 +1,8 @@
 'use strict';
 
-window.app.controller('DoorsController', function () {
+window.app.controller('DoorsController', function ($scope, doorsAccess) {
+
+    $scope.doorsAccess = doorsAccess(false);
+    console.log($scope.doorsAccess);
 
 });
