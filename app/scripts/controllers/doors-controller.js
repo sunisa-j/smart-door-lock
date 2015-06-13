358b2de4756f7f5729e45d48630bc82cfd40e03b
sunisa <sunisa.j@icloud.com>
2015-06-13 17:19:56 +0700

    Edit doors access data name

--- app/scripts/controllers/doors-controller.js
+++ app/scripts/controllers/doors-controller.js
@@ -3,6 +3,6 @@
 window.app.controller('DoorsController', function ($scope, doorsAccess) {
 
     //Get doors user can access informaiton
-    $scope.doorsAccess = doorsAccess('array');
+    $scope.doorsAccessData = doorsAccess('array');
 
 });
