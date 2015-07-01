631e2fa21fe4d899d2862d4373bb5d1523d0bb5f
sunisa <sunisa.j@icloud.com>
2015-07-01 21:45:07 +0700

    Add code about manage access

--- app/scripts/controllers/door-info-controller.js
+++ app/scripts/controllers/door-info-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.app.controller('DoorInfoController', function ($scope, $ionicPopup, $stateParams, doorsAccess, $timeout) {
+window.app.controller('DoorInfoController', function ($scope, $ionicPopup, $stateParams, doorsAccess, passcodeUnlock, $timeout) {
 
     var groupId = $stateParams.groupId;
     var doorId = $stateParams.doorId;
@@ -8,79 +8,56 @@ window.app.controller('DoorInfoController', function ($scope, $ionicPopup, $stat
     $scope.groupName = doorsAccessObj[groupId].name;
     $scope.doorData = doorsAccessObj[groupId].doorsAccess[doorId];
 
-    $scope.pinRequired = { value: false, load: false };
-    $scope.pinRequiredChecked = function(){
-        $scope.pinRequired.load = true;
-        var valueBeforeChanged = !$scope.pinRequired.value;
-
-        // if pinRequired status changed;
-        $scope.pinRequired.value = !(valueBeforeChanged);
-
-        $timeout( function(){
-            if($scope.pinRequired.value == !(valueBeforeChanged)) {
-                $scope.pinRequired.load = false;
-            }
-        }, 1500);
-    };
-
-    $scope.autoRelock = { value: false, load: false };
-    $scope.autoRelockChecked = function(){
-        $scope.autoRelock.load = true;
-        var valueBeforeChanged = !$scope.autoRelock.value;
-
-        // if autoRelock status changed;
-        $scope.autoRelock.value = !(valueBeforeChanged);
-
-        $timeout( function(){
-            if($scope.autoRelock.value == !(valueBeforeChanged)) {
-                $scope.autoRelock.load = false;
-            }
-        }, 1500);
-    }
-
     // Set Default Menu (Menu contains are 'doorInfo', 'configDoor', 'log' and 'manageAccess')
     $scope.doorMenu = {
         name: 'doorInfo'
     };
 
-    // Set Default Display Delete Button
-    $scope.showDelButton = {
+    // About Config Door
+    $scope.pinRequired = { load: false };
+    $scope.autoRelock = { load: false };
+
+    $scope.pinRequiredChecked = function(){
+        $scope.pinRequired.load = true;
+        var valueBeforeChanged = !$scope.doorData.configDoor.pinRequired.status;
+
+        // if pinRequired status changed;
+        $scope.doorData.configDoor.pinRequired.status = !(valueBeforeChanged);
+
+        $timeout( function(){
+            if($scope.doorData.configDoor.pinRequired.status == !(valueBeforeChanged)) {
+                $scope.pinRequired.load = false;
+            }
+        }, 1500);
+    };
+    $scope.autoRelockChecked = function(){
+        $scope.autoRelock.load = true;
+        var valueBeforeChanged = !$scope.doorData.configDoor.autoRelock.status;
+
+        // if autoRelock status changed;
+        $scope.doorData.configDoor.autoRelock.status = !(valueBeforeChanged);
+
+        $timeout( function(){
+            if($scope.doorData.configDoor.autoRelock.status == !(valueBeforeChanged)) {
+                $scope.autoRelock.load = false;
+            }
+        }, 1500);
+    };
+
+    // About Manage Access
+
+    // About Passcode Unlock
+    $scope.passcodeUnlockArr = passcodeUnlock(doorId, '', 'array');
+
+    var passcodeUnlockId = $stateParams.passcodeUnlockId;
+    $scope.passcodeUnlockSelected = passcodeUnlock('', passcodeUnlockId, 'object');
+
+    $scope.enableEditPasscode = {
         value: false
     };
-
-    $scope.deleteSelected = function() {
-        $scope.showDelButton.value = false;
-        console.log('deleted');
+    $scope.passcodeReadOnly = {
+        value: true
     };
 
-    // Confirm Delete Box
-    $scope.confirmDelBox = function() {
 
-        $ionicPopup.show({
-            title: 'Confirm',
-            template: 'Do you confirm to delete all user you selected ?',
-            buttons: [
-                {
-                    text: '<div class="flex align-items-center">' +
-                    '<span class="flex-basis-30">' +
-                    '<i class="button-icon-size ion-ios-close-outline"></i>' +
-                    '</span>' +
-                    '<span class="flex-1">Cancel</span>' +
-                    '</div>',
-                    type: 'button-outline button-stable'
-                },{
-                    text: '<div class="flex align-items-center">' +
-                    '<span class="flex-basis-30">' +
-                    '<i class="button-icon-size ion-ios-minus-outline"></i>' +
-                    '</span>' +
-                    '<span class="flex-1">Delete</span>' +
-                    '</div>',
-                    type: 'button-outline button-assertive',
-                    onTap: function() {
-                        $scope.deleteSelected();
-                    }
-                }
-            ]
-        });
-    };
 });
