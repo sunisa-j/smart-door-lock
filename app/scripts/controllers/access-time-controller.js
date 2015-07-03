58ac7b248618458bebe5ad55a28b0b23018f0a88
sunisa <sunisa.j@icloud.com>
2015-07-03 22:00:51 +0700

    create access-time controller

---
+++ app/scripts/controllers/access-time-controller.js
@@ -0,0 +1,56 @@
+'use strict';
+
+window.app.controller('AccessTimeController', function ($scope, $stateParams, $timeout, $ionicActionSheet, passcodePolicies) {
+
+    var doorId = $stateParams.doorId;
+    var passcodeUnlockId = $stateParams.passcodeUnlockId;
+
+    //Get access time of title informaiton
+    $scope.passcodePoliciesData = passcodePolicies(passcodeUnlockId, 'array');
+
+    // when click delete calendar
+    $scope.deleteCalendar = function (id) {
+        console.log('delete ' + id);
+    };
+
+    // action sheet for select calendar type
+    $scope.selectCalendarType = function() {
+
+        var hideSheet = $ionicActionSheet.show({
+            buttons: [
+                { text: 'Normal' },
+                { text: 'Holiday' }
+            ],
+            //destructiveText: 'Delete',
+            titleText: 'Select Access Policy',
+            cancelText: 'Cancel',
+            cancel: function() {
+                // add cancel code..
+            },
+            buttonClicked: function(index) {
+                if(index === 0) {
+                    $scope.gotoAddCalendars('normal');
+
+                }
+                else if(index === 1) {
+                    $scope.gotoAddCalendars('holiday');
+                }
+                return true;
+            }
+        });
+        //$timeout(function() {
+        //    hideSheet();
+        //}, 4000);
+    };
+
+    $scope.gotoAddCalendars = function (type) {
+        if(type == 'normal') {
+            console.log('select normal access time');
+        }
+        else if(type == 'holiday') {
+            console.log('select holiday access time');
+        }
+    };
+
+
+});
