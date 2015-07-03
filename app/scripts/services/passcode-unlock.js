12aaa38125ff4e37ecd5143b90b26b4741fb2446
sunisa <sunisa.j@icloud.com>
2015-07-03 20:06:51 +0700

    add status in passcode data

--- app/scripts/services/passcode-unlock.js
+++ app/scripts/services/passcode-unlock.js
@@ -6,17 +6,20 @@ window.app.factory('passcodeUnlock', function () {
         'passcode1': {
             name: 'Chinese Student',
             passcode: '123456',
-            door: 'door1'
+            door: 'door1',
+            status: 'Enabled'
         },
         'passcode2': {
             name: 'Trainee',
             passcode: '555555',
-            door: 'door1'
+            door: 'door1',
+            status: 'Enabled'
         },
         'passcode3': {
             name: 'Trainee',
             passcode: '767676',
-            door: 'door2'
+            door: 'door2',
+            status: 'Enabled'
         }
     };
 
@@ -24,7 +27,6 @@ window.app.factory('passcodeUnlock', function () {
 
         // convert object to array
         var passcodeUnlockArr = [];
-
         angular.forEach(passcodeUnlock, function(passcode, key){
             var tmp = angular.copy(passcode);
             if(tmp.door == doorId) {
@@ -33,13 +35,30 @@ window.app.factory('passcodeUnlock', function () {
             }
         });
 
+        // select objects (door: doorId)
+        var passcodeUnlockObj = {};
+        angular.forEach(passcodeUnlock, function(passcode, key){
+            var tmp = angular.copy(passcode);
+            if(tmp.door == doorId) {
+                passcodeUnlockObj[key] = {};
+                passcodeUnlockObj[key].name = tmp.name;
+                passcodeUnlockObj[key].passcode = tmp.passcode;
+                passcodeUnlockObj[key].door = tmp.door;
+                passcodeUnlockObj[key].status = tmp.status;
+            }
+        });
+
         // get passcode unlock
         var passcodeUnlockSelected = passcodeUnlock[passcodeUnlockId];
 
         if(data === 'object'){
-            return passcodeUnlockSelected;
-        }else if (data === 'array') {
+            return passcodeUnlockObj;
+        }
+        else if (data === 'array') {
             return passcodeUnlockArr;
+        }
+        else if (data === 'selected') {
+            return passcodeUnlockSelected;
         }
     };
 });
