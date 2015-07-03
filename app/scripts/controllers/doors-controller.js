aa867a14c90e407e2becd9668b1ff1c18f7c7e24
sunisa <sunisa.j@icloud.com>
2015-07-03 20:05:05 +0700

    add change lock status function

--- app/scripts/controllers/doors-controller.js
+++ app/scripts/controllers/doors-controller.js
@@ -1,8 +1,32 @@
 'use strict';
 
-window.app.controller('DoorsController', function ($scope, doorsAccess) {
+window.app.controller('DoorsController', function ($scope, doorsAccess, $timeout) {
 
     //Get doors user can access informaiton
-    $scope.doorsAccessData = doorsAccess('array');
+    $scope.doorsAccessData = doorsAccess('object');
+
+    $scope.load = {};
+
+    $scope.changeLockStatus = function (e, groupKey, doorKey, latestLockStatus) {
+        e.stopPropagation();
+        $scope.load[doorKey] = true;
+
+        if(latestLockStatus == 'locked') {
+            $scope.doorsAccessData[groupKey].doorsAccess[doorKey].status.lock = 'unlocked';
+            $timeout(function () {
+                if ($scope.doorsAccessData[groupKey].doorsAccess[doorKey].status.lock == 'unlocked') {
+                    $scope.load[doorKey] = false;
+                }
+            }, 1500);
+        }
+        else if(latestLockStatus == 'unlocked') {
+            $scope.doorsAccessData[groupKey].doorsAccess[doorKey].status.lock = 'locked';
+            $timeout(function () {
+                if ($scope.doorsAccessData[groupKey].doorsAccess[doorKey].status.lock == 'locked') {
+                    $scope.load[doorKey] = false;
+                }
+            }, 1500);
+        }
+    };
 
 });
