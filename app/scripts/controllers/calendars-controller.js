7059577956512926a33f1b4b5c621ccbc7dd5eee
sunisa <sunisa.j@icloud.com>
2015-07-09 12:49:54 +0700

    Add create calendar modal

--- app/scripts/controllers/calendars-controller.js
+++ app/scripts/controllers/calendars-controller.js
@@ -1,10 +1,25 @@
 'use strict';
 
-window.app.controller('CalendarsController', function ($scope, usersCalendars) {
+window.app.controller('CalendarsController', function ($scope, usersCalendars, $ionicModal) {
 
     var user = 1; // Login user id
+    $scope.createData = {};
 
     $scope.calendarsData = usersCalendars(user, '', '', '', '');
-    console.log($scope.calendarsData);
+
+    $ionicModal.fromTemplateUrl('templates/create-calendar-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.createCalendarModal = modal;
+    });
+
+    $scope.createCalendar = function () {
+        console.log('Create calendar: ', $scope.createData);
+    };
+    $scope.cancelCreateCalendar = function () {
+        $scope.createData={};
+        $scope.createCalendarModal.hide();
+    };
 
 });
