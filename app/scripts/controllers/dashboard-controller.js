6a9500235f0e790b9dfef658fcaa09d28896ccb6
sunisa <sunisa.j@icloud.com>
2015-07-13 16:17:30 +0700

    Get numberOfDoors & disable calculate expire date

--- app/scripts/controllers/dashboard-controller.js
+++ app/scripts/controllers/dashboard-controller.js
@@ -2,33 +2,37 @@
 
 window.app.controller('DashboardController', function ($scope, doorsAccess) {
 
+    // Login user id
+    var userId = 1;
+
     // get count of access doors & access doors information
-    $scope.doorsAccess = doorsAccess('array');
-    $scope.doorsNumber = doorsAccess('count');
+    $scope.doorsAccess = doorsAccess(userId, 'array');
+    $scope.doorsNumber = doorsAccess(userId, 'array').numberOfDoors;
+    //$scope.doorsNumber = 7;
 
     $scope.dateNow = new Date();
     $scope.getTimeDateNow = $scope.dateNow.getTime();
 
-    $scope.groupDoorsExpireDate = function(doorsAccessData) {
-        var doorsExpireData = [];
-
-        angular.forEach(doorsAccessData, function (group) {
-
-            angular.forEach(group.doorsAccess, function (door) {
-                var tmp = {
-                    doorId: door.$id,
-                    doorName: door.name,
-                    groupId: group.$id,
-                    groupName: group.name,
-                    expireDate: door.expire,
-                    getTimeExpireDate : (door.expire).getTime()
-                };
-                doorsExpireData.push(tmp);
-            });
-        });
-        return doorsExpireData;
-    };
-    $scope.doorsExpireData = $scope.groupDoorsExpireDate($scope.doorsAccess);
+    //$scope.groupDoorsExpireDate = function(doorsAccessData) {
+    //    var doorsExpireData = [];
+    //
+    //    angular.forEach(doorsAccessData, function (group) {
+    //
+    //        angular.forEach(group.doorsAccess, function (door) {
+    //            var tmp = {
+    //                doorId: door.$id,
+    //                doorName: door.name,
+    //                groupId: group.$id,
+    //                groupName: group.name,
+    //                expireDate: door.expire,
+    //                getTimeExpireDate : (door.expire).getTime()
+    //            };
+    //            doorsExpireData.push(tmp);
+    //        });
+    //    });
+    //    return doorsExpireData;
+    //};
+    //$scope.doorsExpireData = $scope.groupDoorsExpireDate($scope.doorsAccess);
 
     // month
     $scope.month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
