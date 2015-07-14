6860903abee7cbffd58eb4bc6e235d29c1f1fd00
sunisa <sunisa.j@icloud.com>
2015-07-14 14:44:51 +0700

    add users service

--- app/scripts/controllers/main-menu-controller.js
+++ app/scripts/controllers/main-menu-controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
-window.app.controller('MainMenuController', function ($scope) {
+window.app.controller('MainMenuController', function ($scope, users) {
+
+    // Login user id
+    var userId = 1;
 
     // get login name
-    $scope.loginName = 'สุนิสา จุลรัตน์';
+    var userData = users('object');
+    $scope.loginName = userData[userId].firstName + ' ' + userData[userId].lastName;
 
     // get count of unread message (notification)
     $scope.unreadMsg = 2;
