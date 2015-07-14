00410dbada732a591485e89c0de6954754758712
sunisa <sunisa.j@icloud.com>
2015-07-14 14:35:09 +0700

    manage array and object data

--- app/scripts/services/passcode-policies-service.js
+++ app/scripts/services/passcode-policies-service.js
@@ -2,24 +2,37 @@
 
 window.app.factory('passcodePolicies', function (calendars) {
 
-    var passcodePolicies = {
-        'passcodePolicy1': {
+    var passcodePolicies = [
+        {
+            id: 'passcodePolicy1',
             type: 'normal',
             passcode: 'passcode1',
             calendar: 'calendar1'
         },
-        'passcodePolicy2': {
+        {
+            id: 'passcodePolicy2',
             type: 'holiday',
             passcode: 'passcode1',
             calendar: 'calendar2'
         },
-        'passcodePolicy3': {
+        {
+            id: 'passcodePolicy3',
             type: 'normal',
             passcode: 'passcode2',
             calendar: 'calendar1'
         }
-    };
+    ];
 
+    // transform passcodePolicies to obj
+    var passcodePoliciesObj = {};
+    angular.forEach(passcodePolicies, function(value){
+        passcodePoliciesObj[value.id] = {};
+        passcodePoliciesObj[value.id].type = value.type;
+        passcodePoliciesObj[value.id].passcode = value.passcode;
+        passcodePoliciesObj[value.id].calendar = value.calendar;
+    });
+
+    // Get Calendars Data
     var calendarsData = calendars;
 
     return function(passcodeUnlockId, data, addObj){
@@ -29,17 +42,17 @@ window.app.factory('passcodePolicies', function (calendars) {
         passcodePoliciesArr[0] = []; // Store Normal Access Time
         passcodePoliciesArr[1] = []; // Store Holiday Access Time
 
-        angular.forEach(passcodePolicies, function(passcodePolicy, key){
+        angular.forEach(passcodePoliciesObj, function(passcodePolicy, key){
 
             var passcodePolicyTmp = angular.copy(passcodePolicy);
             var calendarsTmp = angular.copy(calendarsData);
 
             if(passcodePolicyTmp.type == 'normal' && passcodePolicyTmp.passcode == passcodeUnlockId) {
                 var normalObj = {};
-                normalObj.$id = key;
+                normalObj.id = key;
                 normalObj.type = 'normal',
                 normalObj.calendar = {
-                    $id: passcodePolicy.calendar,
+                    id: passcodePolicy.calendar,
                     public: calendarsTmp[passcodePolicyTmp.calendar].public,
                     name: calendarsTmp[passcodePolicyTmp.calendar].name,
                     description: calendarsTmp[passcodePolicyTmp.calendar].description
@@ -49,10 +62,10 @@ window.app.factory('passcodePolicies', function (calendars) {
             }
             else if(passcodePolicyTmp.type == 'holiday' && passcodePolicyTmp.passcode == passcodeUnlockId) {
                 var holidayObj = {};
-                holidayObj.$id = key;
+                holidayObj.id = key;
                 holidayObj.type = 'holiday',
                 holidayObj.calendar = {
-                    $id: passcodePolicy.calendar,
+                    id: passcodePolicy.calendar,
                     public: calendarsTmp[passcodePolicyTmp.calendar].public,
                     name: calendarsTmp[passcodePolicyTmp.calendar].name,
                     description: calendarsTmp[passcodePolicyTmp.calendar].description
@@ -63,18 +76,18 @@ window.app.factory('passcodePolicies', function (calendars) {
         });
 
         // select objects (passcode: passcodeUnlockId)
-        var passcodePoliciesObj = {};
-        angular.forEach(passcodePolicies, function(passcodePolicy, key){
+        var passcodePoliciesSelectedObj = {};
+        angular.forEach(passcodePoliciesObj, function(passcodePolicy, key){
 
             var passcodePolicyTmp = angular.copy(passcodePolicy);
             var calendarsTmp = angular.copy(calendarsData);
 
             if(passcodePolicyTmp.passcode == passcodeUnlockId) {
-                passcodePoliciesObj[key] = {};
-                passcodePoliciesObj[key].passcode = passcodeUnlockId;
-                passcodePoliciesObj[key].type = passcodePolicyTmp.type;
-                passcodePoliciesObj[key].calendar = {
-                    $id: passcodePolicy.calendar,
+                passcodePoliciesSelectedObj[key] = {};
+                passcodePoliciesSelectedObj[key].passcode = passcodeUnlockId;
+                passcodePoliciesSelectedObj[key].type = passcodePolicyTmp.type;
+                passcodePoliciesSelectedObj[key].calendar = {
+                    id: passcodePolicy.calendar,
                     public: calendarsTmp[passcodePolicyTmp.calendar].public,
                     name: calendarsTmp[passcodePolicyTmp.calendar].name,
                     description: calendarsTmp[passcodePolicyTmp.calendar].description
@@ -82,16 +95,14 @@ window.app.factory('passcodePolicies', function (calendars) {
             }
         });
 
-        if(data == 'object') {
-            return passcodePoliciesObj;
+        if(data === 'object') {
+            return passcodePoliciesSelectedObj;
         }
-        else if(data == 'array') {
+        else if(data === 'array') {
             return passcodePoliciesArr;
         }
-        else if(data == 'add') {
-            passcodePolicies[passcodePolicy.length + 2].type = addObj.type;
-            passcodePolicies[passcodePolicy.length + 2].passcode = addObj.passcode;
-            passcodePolicies[passcodePolicy.length + 2].calendar = addObj.calendar;
+        else if(data === 'add') {
+            console.log('add passcode unlock');
         }
     };
 });
