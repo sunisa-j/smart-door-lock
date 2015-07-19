64ddffe8e30b03900f65de2abec507efb4f646b2
sunisa <sunisa.j@icloud.com>
2015-07-19 15:13:09 +0700

    include ui.calendar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var app = angular.module('SmartDoorLock', ['ionic', 'ui.router.stateHelper']);
+var app = angular.module('SmartDoorLock', ['ionic', 'ui.router.stateHelper', 'ui.calendar']);
 
 app.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
